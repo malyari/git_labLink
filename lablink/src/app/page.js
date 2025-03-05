d00cab60444cdca5148b993e8cc28fa774742595
Maral Alyari <maralalyari@Marals-MBP.localdomain>
2025-03-04 22:38:11 -0700

    updating

--- lablink/src/app/page.js
+++ lablink/src/app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main style={{ textAlign: "center", padding: "50px" }}>
       <h1>🚀 Hello, Scientists! 🧪</h1>
-      <p>Welcome to my test Next.js app using the App Router.</p>
+      <p>Welcome to the app that makes your life easier.</p>
     </main>
   );
 }
